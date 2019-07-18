a3ed485ae4af7d2fa7b1ea50305f62482916cc60
nikita-skobov <nskobov.websites@gmail.com>
2019-07-18 17:26:36 -0500

    adds lesson name as modal title

--- src/components/Lesson.js
+++ src/components/Lesson.js
@@ -5,13 +5,13 @@ import { Modal, ModalHeader, ModalBody } from 'reactstrap'
 import { lessonClosed } from '../actions/lessonActions'
 
 export function Lesson(props) {
-  const { isOpen, lessonClosed: toggle } = props
+  const { isOpen, lessonClosed: toggle, lessonName } = props
   console.log(`rendering lesson with isOpen: ${isOpen}`)
 
   return (
     <div>
       <Modal isOpen={isOpen}>
-        <ModalHeader toggle={toggle}>Title</ModalHeader>
+        <ModalHeader toggle={toggle}>{lessonName}</ModalHeader>
         <ModalBody>
           ayyyy
         </ModalBody>
