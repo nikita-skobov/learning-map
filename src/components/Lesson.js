d7e3791c25fb0720c56be9d3ac511537677ce447
nikita-skobov <nskobov.websites@gmail.com>
2019-07-18 21:10:06 -0500

    adds real data rendering

--- src/components/Lesson.js
+++ src/components/Lesson.js
@@ -1,19 +1,43 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Modal, ModalHeader, ModalBody } from 'reactstrap'
+import katex from 'katex'
 
+import nodeData from '../data/nodeData.json'
 import { lessonClosed } from '../actions/lessonActions'
 
 export function Lesson(props) {
   const { isOpen, lessonClosed: toggle, lessonName } = props
-  console.log(`rendering lesson with isOpen: ${isOpen}`)
+
+  const lessonList = []
+  const lessonObj = nodeData[lessonName]
+
+  if (lessonObj && lessonObj.lesson) {
+    lessonObj.lesson.forEach((obj) => {
+      const [key] = Object.keys(obj)
+      // each object should only have one key
+
+      if (key === 'formula') {
+        const renderedString = katex.renderToString(obj[key], {
+          throwOnError: false,
+        })
+        lessonList.push(
+          <div style={{ textAlign: 'center', margin: '1em' }} dangerouslySetInnerHTML={{ __html: renderedString }} />,
+        )
+      } else {
+        lessonList.push(
+          <p>{obj[key]}</p>,
+        )
+      }
+    })
+  }
 
   return (
     <div>
       <Modal isOpen={isOpen}>
         <ModalHeader toggle={toggle}>{lessonName}</ModalHeader>
         <ModalBody>
-          ayyyy
+          {lessonList}
         </ModalBody>
       </Modal>
     </div>
