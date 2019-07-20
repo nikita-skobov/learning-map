5a9cade628f6dda948381f0405b5727839273cf4
nikita-skobov <nskobov.websites@gmail.com>
2019-07-19 23:18:25 -0500

    moves bootstrap import to top  of index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 /* global document */
 
 import 'react-app-polyfill/ie9'
-// import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 // import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import ReduxApp from './components/App'
 import { createEnhancers, setupStore } from './setupStore'
